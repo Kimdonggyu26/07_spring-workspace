b7b17153dde899161467d4d8a5c1453f7276d069
hiwinter <anima9125@gmail.com>
2024-10-21 15:37:59 +0900

    10/21

--- 03_Spring_Logging_AJAX/src/main/java/com/br/ajax/controller/MemberController2.java
+++ 03_Spring_Logging_AJAX/src/main/java/com/br/ajax/controller/MemberController2.java
@@ -6,52 +6,28 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.br.ajax.dto.MemberDto;
 import com.br.ajax.service.MemberService;
 
 import lombok.RequiredArgsConstructor;
 
-@RequestMapping("/member1")
 @RequiredArgsConstructor
-@Controller
-public class MemberController1 {
-	
+@RequestMapping("/member2")
+@RestController // 안의 메소드들에 전부 @ResponseBody가 붙어진채로 동작한다
+public class MemberController2 {
+
 	private final MemberService memService;
 	private Logger logger = LoggerFactory.getLogger(MemberController1.class);
-	
 
-	// 기존에 HttpServletResponse 방식 이용하는 방법
-	/* 
-	@GetMapping("/detail1.do")
-	public void memberDetail(String id, String pwd, HttpServletResponse response) throws IOException {
-		
-		logger.debug("request id: {}, pwd: {}", id, pwd);
-		
-		String result = memService.selectMemberByIdPwd(id, pwd);
-		
-		response.setContentType("text/html; charset=utf-8");
-		PrintWriter out = response.getWriter();
-		out.print(result);
-		
-	}
-	*/
-	
-	/*
-	 * * @ResponseBody
-	 * - 비동기식으로 데이터 응답시 필요한 어노테이션
-	 * - 해당 어노테이션이 붙은 메소드에서의 반환 값은
-	 * 	 응답뷰가 아닌 어떤 data(text, json, xml 등)라는 걸 의미
-	 */
-	
-	@ResponseBody
+	//@ResponseBody
 	@GetMapping(value="/detail1.do", produces="text/html; charset=utf-8")
 	public String memberDetail1(String id, String pwd) {
 		
@@ -62,7 +38,7 @@ public class MemberController1 {
 		return result;
 	}
 	
-	@ResponseBody
+	//@ResponseBody
 	@GetMapping(value="/detail2.do", produces="text/html; charset=utf-8")
 	public String memberDetail2(String userId, String userPwd) {
 		// String result = memService.selectMemberByIdPwd(userId, userPwd);
@@ -73,7 +49,7 @@ public class MemberController1 {
 	 * 자바 객체 (Dto, List, Map, 배열 등)를 응답할때는 json 데이터로 변환해서 응답해야됨
 	 * 자동으로 변환 처리해주는 jackson 라이브러리 등록하면 바로 반환만 하면됨
 	 */
-	@ResponseBody
+	//@ResponseBody
 	@GetMapping(value="/detail3.do", produces="application/json")
 	public MemberDto memberDetail3(@RequestParam(value="no", defaultValue="1") int userNo) {
 		MemberDto mem = memService.selectMemberByNo(userNo);
@@ -81,7 +57,7 @@ public class MemberController1 {
 		return mem;	//{필드명:필드값, 필드명:필드값 ....}
 	}
 	
-	@ResponseBody
+	//@ResponseBody
 	@GetMapping(value="list.do", produces="application/json")
 	public List<MemberDto> memberList() {
 		List<MemberDto> list = memService.selectMemberList();
@@ -89,7 +65,7 @@ public class MemberController1 {
 		return list; // [{}, {}, {}]
 	}
 	
-	@ResponseBody
+	//@ResponseBody
 	@GetMapping(value="etc1.do")
 	public Map<String, Object> responseMapTest() {
 		// 만일 응답할 데이터로 숫자, List, Dto가 있다는 가정하에
@@ -101,7 +77,7 @@ public class MemberController1 {
 		return map;
 	}
 	
-	@ResponseBody
+	//@ResponseBody
 	@PostMapping(value="etc2.do")
 	public void requestBodyTest(@RequestBody Map<String, Object> map) {
 		logger.debug("map: {}", map);
@@ -109,14 +85,4 @@ public class MemberController1 {
 		logger.debug("map>name: {}", map.get("name"));
 		logger.debug("map>arr: {}", map.get("arr"));
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 }
